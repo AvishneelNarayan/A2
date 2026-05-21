77f1044e789f7f4a318dd952036e511bb5a89298
AvishneelNarayan <anar0025@student.monash.edu>
2026-05-22 05:45:27 +1000

    Make network map routes visible

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
 const charts = [
-  ["#network_map", "js/vega/01_network_map.json"],
-  ["#stop_point_map", "js/vega/02_stop_point_map.json"],
-  ["#mode_counts_bar", "js/vega/03_mode_counts_bar.json"],
-  ["#stop_density_choropleth", "js/vega/04_stop_density_choropleth.json"],
-  ["#mode_small_multiples", "js/vega/05_mode_small_multiples.json"],
-  ["#mode_mix_stacked_bar", "js/vega/06_mode_mix_stacked_bar.json"],
-  ["#mode_coverage_dotplot", "js/vega/07_mode_coverage_dotplot.json"],
-  ["#population_access_choropleth", "js/vega/08_population_access_choropleth.json"],
-  ["#population_vs_stops_scatter", "js/vega/09_population_vs_stops_scatter.json"],
-  ["#ranked_access_bar", "js/vega/10_ranked_access_bar.json"],
-  ["#access_score_choropleth", "js/vega/11_access_score_choropleth.json"],
-  ["#hourly_service_heatmap", "js/vega/12_hourly_service_heatmap.json"]
+  ["#network_map", "js/vega/01_network_map.json?v=20260522b"],
+  ["#stop_point_map", "js/vega/02_stop_point_map.json?v=20260522b"],
+  ["#mode_counts_bar", "js/vega/03_mode_counts_bar.json?v=20260522b"],
+  ["#stop_density_choropleth", "js/vega/04_stop_density_choropleth.json?v=20260522b"],
+  ["#mode_small_multiples", "js/vega/05_mode_small_multiples.json?v=20260522b"],
+  ["#mode_mix_stacked_bar", "js/vega/06_mode_mix_stacked_bar.json?v=20260522b"],
+  ["#mode_coverage_dotplot", "js/vega/07_mode_coverage_dotplot.json?v=20260522b"],
+  ["#population_access_choropleth", "js/vega/08_population_access_choropleth.json?v=20260522b"],
+  ["#population_vs_stops_scatter", "js/vega/09_population_vs_stops_scatter.json?v=20260522b"],
+  ["#ranked_access_bar", "js/vega/10_ranked_access_bar.json?v=20260522b"],
+  ["#access_score_choropleth", "js/vega/11_access_score_choropleth.json?v=20260522b"],
+  ["#hourly_service_heatmap", "js/vega/12_hourly_service_heatmap.json?v=20260522b"]
 ];
 
 const embedOptions = {
